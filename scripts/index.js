fd3c85ff214a6e9065328fbae523f2815c49d955
juheekimmdesign <juheekimmdesign@gmail.com>
2026-04-02 18:46:58 +0900

    2

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
 // index.js
+const topBnr = document.querySelector('.top_bnr_swiper .top_bnr')
+const topBnrSwiper = new Swiper(topBnr, {
+    effect:'fade',
+    loop:true,
+    autoplay:{delay:5000,},
+})
+console.log(topBnr);
